81e70e91293a22b7b1cb118afc417ad91369fb33
1000ch <shogo.sensui@gmail.com>
2020-01-02 11:01:23 +0900

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    dir: 'dist',
+    format: 'esm'
+  },
+  plugins: [
+    typescript({
+      typescript: require('typescript'),
+      tslib: fs.readFileSync(require.resolve('tslib'))
+    }),
+    commonjs(),
+    resolve()
+  ]
+};
---
+++ test/index.test.ts
@@ -0,0 +1,7 @@
+import XMarked from '../src/index';
+
+describe('XMarked', (): void => {
+  test('should be defined', (): void => {
+    expect(XMarked).toBeDefined();
+  });
+})
